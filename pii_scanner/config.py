efb3fbf6f65cf501d76313c99654eb44c5a4d545
Dwaipayan Mistry <dwaip.mistry@gmail.com>
2025-07-13 21:17:43 +0530

    Fixes some bus while scanning

--- pii_scanner/config.py
+++ pii_scanner/config.py
@@ -1,5 +1,3 @@
-import os
-
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DB_DIALECTS = ['oracle', 'mysql', 'postgresql']
--- pii_scanner/utils/pii_detector.py
+++ pii_scanner/utils/pii_detector.py
@@ -1,13 +1,22 @@
-from .regex_rules import PII_PATTERNS
 import re
+from sqlalchemy import text
+from .regex_rules import PII_PATTERNS
+
+def get_sample_query(table_name, engine):
+    dialect = str(engine.url.get_backend_name())
+    if 'oracle' in dialect:
+        return text(f'SELECT * FROM "{table_name}" WHERE ROWNUM <= 1000')
+    else:
+        return text(f'SELECT * FROM "{table_name}" LIMIT 1000')
 
 def scan_table(engine, table_name):
     results = []
     with engine.connect() as conn:
-        query = f'SELECT * FROM "{table_name}" LIMIT 1000'  # Avoid scanning huge data
+        query = get_sample_query(table_name, engine)
         result_proxy = conn.execute(query)
         rows = result_proxy.fetchall()
-        columns = result_proxy.keys()
+        columns = columns = list(result_proxy.keys())  # Now indexable
+
 
         for row in rows:
             for idx, value in enumerate(row):
