6ceebbf48710e5f4ff9c9a474766879f14a00328
Dwaipayan Mistry <dwaip.mistry@gmail.com>
2025-07-14 12:07:06 +0530

    Adds frontend to db, and liks with express app

--- pii_scanner/app.py
+++ pii_scanner/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from config import Config
 from routes.api_routes import routes
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])  # ✅ Allow React frontend
+CORS(app)  # Allow all origins (for local testing)
 
 app = Flask(__name__)
 app.config.from_object(Config)
